7c2099ee191c4458c572d6b0c22e3fddb856e9e5
poetadelvino <almanegraecm@gmail.com>
2015-04-21 20:36:39 -0700

    justTesting

---
+++ TestingThrowAway/TestingThrowAway/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  TestingThrowAway
+//
+//  Created by ernesto on 4/21/15.
+//  Copyright (c) 2015 Pyramidia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.greetings()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func greetings() {
+        
+        
+        println("hello")
+    }
+
+}
+
